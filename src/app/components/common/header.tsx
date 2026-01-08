8d982e2fed0470c4ea1eb773dce3b4c70ede70ba
Razan M. Fahmi <razan_assem@hotmail.com>
2026-01-08 16:41:20 +0200

    change: header color

--- src/app/components/common/header.tsx
+++ src/app/components/common/header.tsx
@@ -3,7 +3,9 @@ import DarkThemeToggle from "./dark-theme-toggle";
 
 export default function Header() {
   return (
-    <div className="sticky h-20 w-full bg-white py-5 shadow-md shadow-zinc-100 dark:bg-slate-600 dark:shadow-none lg:h-16">
+    <div className="sticky h-20 w-full bg-blue-300 py-5 shadow-md shadow-zinc-100 dark:bg-slate-600 dark:shadow-none lg:h-16">
+      {/* bg-white */}
+
       <div className="flex justify-between px-14">
         <h1 className="text-xl font-bold dark:text-zinc-200">
           <Link href="/">Where in the world?</Link>
